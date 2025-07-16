83fa61666aba4a72a27862da8c64c8fc95c01590
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-07-16 21:42:20 +0530

    configed the storage with appwrite. methods created and learned - create file -delete file - get file preview

---
+++ src/appwrite/storageConfig.js
@@ -0,0 +1,81 @@
+import config from "../config/config.js";
+import { Client, ID, Storage } from "appwrite";
+
+const client = new Client()
+  .setEndpoint(config.appwriteURL)
+  .setProject(config.appwriteProjectId);
+
+const storage = new Storage(client);
+
+// This function upload's the image and returns the URL.
+export const uploadFile = async (file) => {
+  try {
+    const uploadedFile = await storage.createFile(
+      config.appwriteBucketId,
+      ID.unique(),
+      file
+    );
+    console.log("appwrite :: storage :: uploadfile", uploadedFile);
+    return {
+      success: true,
+      data: uploadedFile,
+    };
+  } catch (error) {
+    console.error("appwrite :: storage :: uploadFile", error);
+
+    return {
+      success: false,
+      error: {
+        message: error?.message || "failed to upload file",
+        code: error?.code || "UPLOADFILE FAILED",
+      },
+    };
+  }
+};
+
+// This function deletes the file whose ID is passed.
+export const deleteFile = async (fileId) => {
+  try {
+    const deletedFile = await storage.deleteFile(
+      config.appwriteBucketId,
+      fileId
+    );
+
+    console.log("appwrite :: storage :: deledeFile", deletedFile);
+    return {
+      success: true,
+      data: deletedFile,
+    };
+  } catch (error) {
+    console.error("appwrite :: storage :: deleteFile", error);
+
+    return {
+      success: false,
+      error: {
+        message: error?.message || "Failed to delteFile",
+        code: code?.code || "DELETEFILE_FAILED",
+      },
+    };
+  }
+};
+
+// This function gets the file for preview
+export const getFilePreview = (fileId) => {
+  try {
+    const file = storage.getFilePreview(config.appwriteBucketId, fileId);
+    console.log("appwrite :: storage :: getFilePreview", file);
+    return {
+      success: true,
+      data: file,
+    };
+  } catch (error) {
+    console.log("appwrite :: storage :: getFilePreview", error);
+    return {
+      success: false,
+      error: {
+        message: error?.message || "Failed to get the preview of the file",
+        code: error?.code || "GETFILEPREVIEW_FAILED",
+      },
+    };
+  }
+};
